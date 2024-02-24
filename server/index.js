27440299483bd8e9409135eac79804b5f8511c2c
mingmoth <mingmoth@gmail.com>
2024-02-24 22:10:50 +0800

    chore: remove express-session & passport-local package

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import path from 'path'
-import session from 'express-session'
 import router from './routes/index.js'
 import assets from './assets/index.js'
 
@@ -11,14 +10,6 @@ const port = 3000
 const publicPath = path.join(path.resolve(), "public")
 app.use('/', express.static(publicPath))
 app.use("/src", assets)
-
-app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 600 * 1000 }, //10分鐘到期
-    httpOnly: true,
-}))
 
 app.use(bodyParser.json());
 
