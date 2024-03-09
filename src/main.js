322d7248ac06e44757236a401a0ead9ef6f03ca6
mingmoth <mingmoth@gmail.com>
2024-03-09 16:48:31 +0800

    Updates

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { router } from './routes'
-import './style.css'
-import App from './App.vue'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
